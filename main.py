4fd29501ecc6b0ac37592baca7b46499a0d83ed9
a-ksharma <ay1311ush@gmail.com>
2026-06-18 00:52:10 +0530

    Updated the Webhook URL

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import asyncio
 from telegram.ext import ApplicationBuilder
 from telegram.ext import MessageHandler, filters
 from handlers.message_handler import handle_message
 from handlers.onboarding_handler import get_onboarding_handler
 from config import TELEGRAM_BOT_TOKEN, PORT, WEBHOOK_URL
 
-async def main():
+def main():
     app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     app.add_handler(get_onboarding_handler())
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
@@ -13,4 +12,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()

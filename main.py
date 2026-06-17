6d376a7abeff76e443be5051e3308bcf36d7960f
a-ksharma <ay1311ush@gmail.com>
2026-06-18 00:47:33 +0530

    Updated the Webhook URL

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ async def main():
     app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     app.add_handler(get_onboarding_handler())
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-    await app.bot.set_webhook(url=f"{WEBHOOK_URL}/webhook")
-    app.run_webhook(listen="0.0.0.0",port=PORT,webhook_path="/webhook")
+    app.run_webhook(listen="0.0.0.0",port=PORT,webhook_url=f"{WEBHOOK_URL}/webhook")
 
 
 if __name__ == "__main__":
